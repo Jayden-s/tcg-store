7239b7decc5b8be412682bfe3ee3c1874338b091
Jayden Smith <jaydens19@icloud.com>
2022-10-11 22:24:16 +1100

    css

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,7 @@ import Layout from '../components/layout'
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <Layout container>
         <p>I'm making this by following the Gatsby Tutorial.</p>
-      </Layout>
     </Layout>
   )
 }
