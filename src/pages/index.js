2316b20e13370db0b2b3e5101154dc065d15aa3c
Jayden Smith <jaydens19@icloud.com>
2022-10-13 12:03:10 +1100

    bg color test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,16 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/layout'
+import { Global } from '@emotion/core'
 
 const IndexPage = () => {
   return (
-    <Layout pageTitle="Home Page">
-        <p>I'm making this by following the Gatsby Tutorial.</p>
-    </Layout>
+    <>
+    <Global />
+      <Layout pageTitle="Home Page">
+         <p>I'm making this by following the Gatsby Tutorial.</p>
+      </Layout>
+    </>
   )
 }
 
